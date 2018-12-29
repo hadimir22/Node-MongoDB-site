64f11a0f179c4d33a450590cfeafa48fd98bf41a
Hadi Mir <hmir@sonicwall.com>
2018-12-29 22:20:09 +0530

    done

--- test.js
+++
@@ -1,7 +0,0 @@
-var responseText = {
-                    {"presubcatId":"1","precatId":"1","presubcatName":"HR Manager"},
-                   {"presubcatId":"2","precatId":"1","presubcatName":"Marketing Manager"},
-                }
-
-var subcats = JSON.parse(this.responseText);
-console.log(subcats);
